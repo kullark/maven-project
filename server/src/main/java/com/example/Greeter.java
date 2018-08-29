014e9aaad9724a15a35f1e98d68a9677ac4a1aea
spocklar <spocklar@gmail.com>
2018-08-29 12:20:27 -0700

    fix comment in greeter

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,7 @@ public class Greeter {
   }
 
   /**
-   *  ( @param someone the name of a person
-   * @param someone
+   * @param someone the name of a person.
    * @return greeting string
    */
 
